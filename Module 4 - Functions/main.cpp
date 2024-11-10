0a06e41106e29d3e4d0ac90dfe87a3f59f4438ab
Niki Namian <nikinamian@gmail.com>
2024-11-10 11:28:06 -0800

    Initial Commit

---
+++ Module 4 - Functions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Module 4 - Functions
+//
+//  Created by Niki Namian on 11/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
